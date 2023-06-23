f09e893fbb25e134f28d3b903b09242a1241f4dd
Shall <xhall.mc@protonmail.com>
2023-06-23 17:51:19 -0600

    Update version - Set to a pre-release.

--- dataconverter/__init__.py
+++ dataconverter/__init__.py
@@ -2,4 +2,4 @@ from dataconverter.bytesconvert import bytes_converter, data_to_bytes
 from dataconverter.convert import  to_kb, to_mb, to_gb, to_tb
 # from cliwiper.cliwiper import wiper
 
-__version__ = "0.1.0"
+__version__ = "1.0.0rc0"
